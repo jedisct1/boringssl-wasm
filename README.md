380246acc93e65ce1daac4112ed99951acf49bea
Frank Denis <github@pureftpd.org>
2022-10-04 00:27:20 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,77 @@
+# BoringSSL (boringcrypto) for WebAssembly
+
+A zig build file to compile BoringSSL's `libcrypto` to WebAssembly/WASI.
+
+## Dependencies
+
+The only required dependencies to build the library are:
+
+* [Go](https://www.golang.org) - Required by BoringSSL to generate the error codes map
+* [Zig](https://www.ziglang.org) - To compile C/C++ code to WebAssembly
+
+## BoringSSL submodule
+
+This repository includes an unmodified version of `BoringSSL` as a submodule. If you didn't clone it with the `--recursive` flag, the following command can be used to pull the submodule:
+
+```sh
+git submodule update --init --recursive --depth=1
+```
+
+## Building the BoringSSL crypto library for WebAssembly
+
+Generic build for WebAssembly/WASI:
+
+```sh
+zig build -Dtarget=wasm32-wasi -Drelease-fast
+```
+
+The resulting static library is put into `zig-out/libcrypto.a`.
+
+Build modes:
+
+* `-Drelease-fast`
+* `-Drelease-safe`
+* `-Drelease-small` (also turns `OPENSSL_SMALL` to disable precomputed tables)
+* `-Ddebug` (default, not recommended in production builds)
+
+The resulting library is compatible with the vast majority of WebAssembly runtimes.
+
+Optimizations only compatible with some runtimes can be also enabled:
+
+```sh
+zig build -Dtarget=wasm32-wasi -Drelease-fast \
+          -Dcpu=generic+simd128+multivalue+bulk_memory
+```
+
+Possibly relevant extensions:
+
+* `bulk_memory`
+* `exception_handling`
+* `multivalue`
+* `sign_ext`
+* `simd128`
+* `tail_call`
+
+## Cross-compiling to other targets
+
+The build file can be used to cross-compile to other targets as well. However, assembly implementations will not be included.
+
+### Examples
+
+Compile to the native architecture:
+
+```sh
+zig build -Dtarget=native -Drelease-fast
+```
+
+Cross-compile to `x86_64-linux`:
+
+```sh
+zig build -Dtarget=x86_64-linux -Drelease-small
+```
+
+Cross-compile to Apple Silicon:
+
+```sh
+zig build -Dtarget=aarch64-macos -Drelease-safe
+```
---
+++ build.zig
@@ -0,0 +1,109 @@
+const std = @import("std");
+const fs = std.fs;
+const heap = std.heap;
+const mem = std.mem;
+const ArrayList = std.ArrayList;
+
+const path_boringssl = "boringssl";
+
+fn withBase(alloc: mem.Allocator, base: []const u8, name: []const u8) !ArrayList(u8) {
+    var path = ArrayList(u8).init(alloc);
+    try path.appendSlice(base);
+    try path.append(fs.path.sep);
+    try path.appendSlice(name);
+    return path;
+}
+
+fn buildErrData(alloc: mem.Allocator, lib: *std.build.LibExeObjStep, base: []const u8) !void {
+    const out_name = "err_data_generate.c";
+    var arena = heap.ArenaAllocator.init(alloc);
+    defer arena.deinit();
+
+    var dir = try fs.cwd().makeOpenPath(base, .{});
+    defer dir.close();
+    var fd = try dir.createFile(out_name, .{});
+    defer fd.close();
+
+    var child = std.ChildProcess.init(
+        &.{ "go", "run", path_boringssl ++ "/crypto/err/err_data_generate.go" },
+        arena.allocator(),
+    );
+    child.stdout_behavior = .Pipe;
+    try child.spawn();
+    try fd.writeFileAll(child.stdout.?, .{});
+    _ = try child.wait();
+
+    const path = try withBase(arena.allocator(), base, out_name);
+    lib.addCSourceFile(path.items, &.{});
+}
+
+fn addDir(alloc: mem.Allocator, lib: *std.build.LibExeObjStep, base: []const u8) !void {
+    var arena = heap.ArenaAllocator.init(alloc);
+    defer arena.deinit();
+
+    var dir = try fs.cwd().openIterableDir(base, .{});
+    defer dir.close();
+    var it = dir.iterate();
+    while (try it.next()) |file| {
+        if (!mem.eql(u8, fs.path.extension(file.name), ".c")) {
+            continue;
+        }
+        const path = try withBase(arena.allocator(), base, file.name);
+        lib.addCSourceFile(path.items, &.{});
+    }
+}
+
+fn addSubdirs(alloc: mem.Allocator, lib: *std.build.LibExeObjStep, base: []const u8) !void {
+    var arena = heap.ArenaAllocator.init(alloc);
+    defer arena.deinit();
+
+    var dir = try fs.cwd().openIterableDir(base, .{});
+    defer dir.close();
+    var it = dir.iterate();
+    while (try it.next()) |file| {
+        if (file.kind != .Directory) {
+            continue;
+        }
+        const path = try withBase(arena.allocator(), base, file.name);
+        try addDir(alloc, lib, path.items);
+    }
+}
+
+pub fn build(b: *std.build.Builder) !void {
+    var gpa = heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+
+    const lib = b.addStaticLibrary("crypto", null);
+    lib.setBuildMode(mode);
+    lib.setTarget(target);
+    lib.strip = true;
+    lib.linkLibC();
+    lib.install();
+    if (mode == .ReleaseSmall) {
+        lib.defineCMacro("OPENSSL_SMALL", null);
+    }
+
+    lib.defineCMacro("ARCH", "generic");
+    lib.defineCMacro("OPENSSL_NO_ASM", null);
+
+    if (target.os_tag) |tag| {
+        if (tag == .wasi) {
+            lib.defineCMacro("OPENSSL_NO_THREADS_CORRUPT_MEMORY_AND_LEAK_SECRETS_IF_THREADED", null);
+            lib.defineCMacro("SO_KEEPALIVE", "0");
+            lib.defineCMacro("SO_ERROR", "0");
+            lib.defineCMacro("FREEBSD_GETRANDOM", null);
+            lib.defineCMacro("getrandom(a,b,c)", "getentropy(a,b)|b");
+            lib.defineCMacro("GRND_NONBLOCK", "0");
+        }
+    }
+
+    lib.addIncludePath(path_boringssl ++ fs.path.sep_str ++ "include");
+    const base_crypto = path_boringssl ++ fs.path.sep_str ++ "crypto";
+    const base_generated = "generated";
+    try buildErrData(gpa.allocator(), lib, base_generated);
+    try addDir(gpa.allocator(), lib, base_crypto);
+    try addSubdirs(gpa.allocator(), lib, base_crypto);
+}
